44a6b1555a695ef779ec9e660aad0e7364139e1a
Nina Socrate <ninasocrate@MacBook-Pro-de-Nina.local>
2025-11-04 11:34:40 +0100

    first commit

---
+++ backend/huproject/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Caregiver, Recipient
+
+# Register your models here.
+
+@admin.register(Caregiver)
+class CaregiverAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'user', 'access_level')
+    search_fields = ('first_name', 'last_name', 'user__username')
+    list_filter = ('access_level',)
+
+
+@admin.register(Recipient)
+class RecipientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name')
+    search_fields = ('first_name', 'last_name')
+    filter_horizontal = ('caregivers',)  # nice UX for ManyToMany

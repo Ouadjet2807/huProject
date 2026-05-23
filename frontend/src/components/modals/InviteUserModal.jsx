01ffeef2236de945054c3a3ce8319a40062d4b0d
Wadjet <n.socrate@outlook.com>
2026-05-23 19:17:15 +0200

    debugging invite user component

--- frontend/src/components/modals/InviteUserModal.jsx
+++ frontend/src/components/modals/InviteUserModal.jsx
@@ -49,7 +49,7 @@ export default function InviteUserModal({ show, setShow }) {
             console.log('Success', post);
         }
         catch (error) {
-            console.log(error);
+            console.log(error.response);
         }
     }
 

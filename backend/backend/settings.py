900c4d18047083c8412adf61b9f71f37d92d9f9b
Wadjet <n.socrate@outlook.com>
2026-05-26 12:19:37 +0200

    setting redis up for production

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -198,8 +198,8 @@ EMAIL_TIMEOUT = int(os.getenv("EMAIL_TIMEOUT"))
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = "redis://redis:6379/0"
-CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+CELERY_BROKER_URL = os.getenv("REDIS_URL")
+CELERY_RESULT_BACKEND = os.getenv("REDIS_URL")
 
 CELERY_TIMEZONE = 'UTC'
 

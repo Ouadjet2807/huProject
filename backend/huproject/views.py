45ce4272eb0543d9b3b0fe9d1e156360dc7740b5
Wadjet <n.socrate@outlook.com>
2026-06-13 21:41:16 +0200

    fixing logout func

--- backend/huproject/views.py
+++ backend/huproject/views.py
@@ -74,10 +74,8 @@ class UserLogoutAPIView(GenericAPIView):
 
     def post(self, request, *args, **kwargs):
         try:
-            refresh_token = request.data["refresh"]
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-            return Response(status=status.HTTP_205_RESET_CONTENT)
+            request.user.auth_token.delete()
+            return Response({"message": "Logged out successfully"}, status=200)
         except Exception as e:
             return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
 
